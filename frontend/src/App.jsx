100e96c414a50954a77995bd5398ee6d5699347b
meghana <meghana.mahendarkar@gmail.com>
2026-08-14 11:37:01 +0530

    final

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [error, setError] = useState("");
   const fileRef = useRef(null);
 
-  const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+  const API_URL = "addhttps://askmynotes-api-712f0.containers.snapdeploy.app";
 
   const askQuestion = async () => {
     const cleanedQuestion = question.trim();
